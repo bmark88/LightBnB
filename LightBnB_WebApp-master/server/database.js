9d99007137197b2167003b61bbaaccba5593edfe
bmark88 <bradley.mark@hotmail.com>
2020-04-02 15:19:30 +0000

    add pool.queries into javascript functions

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -16,16 +16,22 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+  const queryString = `
+  SELECT * 
+  FROM users
+  WHERE email = $1;
+  `;
+  
+  return pool
+    .query(queryString, [email])
+    .then(res => {
+      if (res.rows.length === 0) {
+        return null
+       }
+        
+       return res.rows[0];
+       })
+    .catch(e => console.error('query error', e.stack));
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -35,7 +41,22 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  const queryString = `
+  SELECT * 
+  FROM users
+  WHERE id = $1;
+  `;
+  
+  return pool
+    .query(queryString, [id])
+    .then(res => {
+      if (res.rows.length === 0) {
+        return null;
+       }
+         
+       return res.rows[0];
+      })
+    .catch(e => console.error('query error', e.stack));
 }
 exports.getUserWithId = getUserWithId;
 
@@ -46,10 +67,20 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  const name = user.name;
+  const email = user.email;
+  const password = user.password;
+
+  const queryString = `
+  INSERT INTO users (name, email, password)
+  VALUES ($1, $2, $3)
+  RETURNING *;
+  `;
+  
+  return pool
+    .query(queryString, [name, email, password])
+    .then(res => res.rows)
+    .catch(e => console.error('query error', e.stack));
 }
 exports.addUser = addUser;
 
